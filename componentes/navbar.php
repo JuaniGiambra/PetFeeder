26e3de17656d21b599a2d9a5030d7ef4b64eea2a
Juani Giambra <GiambraJuan@gmail.com>
2017-09-26 13:37:07 -0300

    Componentes

--- componentes/navbar.php
+++ componentes/navbar.php
@@ -6,13 +6,13 @@
   <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
   <ul class="right hide-on-med-and-down">
    <li><a href="index.php">Inicio</a></li>
-   <li><a href="p/soporte.html">Soporte</a></li>
+   <li><a href="p/soporte.php">Soporte</a></li>
    <li><a href="p/infoprod.html">Que es</a></li>
    <li><a class="waves-effect waves-light btn" href="p/login.php">Iniciar Sesion</a></li>
  </ul>
  <ul class="side-nav" id="mobile-demo">
    <li><a href="index.php">Inicio</a></li>
-   <li><a href="p/soporte.html">Soporte</a></li>
+   <li><a href="p/soporte.php">Soporte</a></li>
    <li><a href="p/infoprod.html">Que es</a></li>
    <li><a class="waves-effect waves-light btn" href="p/login.php">Iniciar Sesion</a></li>
  </ul>
